df0d1e3c17e7ef79c21bc2af4b472a1a0cc3178c
alex-isidro <alexander.d.isidro@gmail.com>
2026-03-15 12:50:55 -0300

    feat(domain): Adiciona as propriedades Active e CreatedAt BaseEntity.cs.

--- ClothingStore.Domain/Commom/BaseEntity.cs
+++ ClothingStore.Domain/Commom/BaseEntity.cs
@@ -4,4 +4,7 @@ public abstract class BaseEntity
 {
     public Guid Id { get; private set; } = Guid.NewGuid();
     
+    public bool Active { get; protected set; } = true;
+
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
 }
