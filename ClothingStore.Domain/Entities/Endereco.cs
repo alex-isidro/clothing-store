c8881a68f719e37ad2de8d0b69eb807ca2d47c2b
KelsonZh0 <kel180498@gmial.com>
2026-03-14 22:24:28 -0300

    feat(domain): adiciona atributos e construtor na entidade Endereco

---
+++ ClothingStore.Domain/Entities/Endereco.cs
@@ -0,0 +1,35 @@
+﻿using ClothingStore.Domain.Commom;
+
+namespace ClothingStore.Domain.Entities;
+
+public class Endereco : BaseEntity
+{
+    public Guid ClienteId { get; private set; }
+    public string Logradouro { get; private set; }
+    public string Numero { get; private set; }
+    public string Complemento { get; private set; }
+    public string Bairro { get; private set; }
+    public string Cidade { get; private set; }
+    public string Estado { get; private set; }
+    public string Cep { get; private set; }
+
+    public Endereco(
+        Guid clienteId,
+        string logradouro,
+        string numero,
+        string complemento,
+        string bairro,
+        string cidade,
+        string estado,
+        string cep)
+    {
+        ClienteId = clienteId;
+        Logradouro = logradouro;
+        Numero = numero;
+        Complemento = complemento;
+        Bairro = bairro;
+        Cidade = cidade;
+        Estado = estado;
+        Cep = cep;
+    }
+}
