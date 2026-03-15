f16569057639b3163730e03a22ba8ad37ff6bc7d
KelsonZh0 <kel180498@gmial.com>
2026-03-14 22:25:41 -0300

    feat(domain): adiciona atributos, relacionamentos e construtor validado na entidade Produto

--- ClothingStore.Domain/Entities/Produto.cs
+++ ClothingStore.Domain/Entities/Produto.cs
@@ -1,12 +1,51 @@
-﻿using ClothingStore.Domain.Common;
+﻿using ClothingStore.Domain.Commom;
 
 namespace ClothingStore.Domain.Entities;
 
 public class Produto : BaseEntity
 {
-    public string Nome { get; set; }
-    public string Descricao { get; set; }
-    public decimal Preco { get; set; }
-    public string Tamanho { get; set; }
-    public string Cor { get; set; }
+    public Guid MarcaId { get; private set; }
+    public Guid CategoriaId { get; private set; }
+    public string Nome { get; private set; }
+    public string Descricao { get; private set; }
+    public decimal Preco { get; private set; }
+    public string Tamanho { get; private set; }
+    public string Cor { get; private set; }
+    
+    public Estoque? Estoque { get; private set; }
+    
+    public List<ItemPedido> ItensPedido { get; private set; }
+
+    public Produto(Guid marcaId, Guid categoriaId, string nome, string descricao, decimal preco, string tamanho, string cor)
+    {
+        if (marcaId == Guid.Empty)
+            throw new Exception("MarcaId não pode ser vazio.");
+
+        if (categoriaId == Guid.Empty)
+            throw new Exception("CategoriaId não pode ser vazio.");
+
+        if (string.IsNullOrWhiteSpace(nome))
+            throw new Exception("Nome não pode ser vazio.");
+
+        if (string.IsNullOrWhiteSpace(descricao))
+            throw new Exception("Descrição não pode ser vazia.");
+
+        if (preco < 0)
+            throw new Exception("Preço não pode ser negativo.");
+
+        if (string.IsNullOrWhiteSpace(tamanho))
+            throw new Exception("Tamanho não pode ser vazio.");
+
+        if (string.IsNullOrWhiteSpace(cor))
+            throw new Exception("Cor não pode ser vazia.");
+
+        MarcaId = marcaId;
+        CategoriaId = categoriaId;
+        Nome = nome;
+        Descricao = descricao;
+        Preco = preco;
+        Tamanho = tamanho;
+        Cor = cor;
+        ItensPedido = new List<ItemPedido>();
+    }
 }
