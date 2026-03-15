bfc2ea7f79c7abfea45dc8f3f9d70cd9facc0f32
KelsonZh0 <kel180498@gmial.com>
2026-03-14 22:22:59 -0300

    feat(domain): adiciona atributos e construtor validado na entidade Cliente

--- ClothingStore.Domain/Entities/Cliente.cs
+++ ClothingStore.Domain/Entities/Cliente.cs
@@ -1,20 +1,38 @@
-﻿using ClothingStore.Domain.Common;
+using ClothingStore.Domain.Commom;
 
 namespace ClothingStore.Domain.Entities;
 
 public class Cliente : BaseEntity
 {
-    public string Nome { get; set; }
-
-    public string Cpf { get; set; }
-
-    public string Email { get; set; }
-
-    public string Telefone { get; set; }
+    public string Nome { get; private set; }
+    public string Cpf { get; private set; }
+    public string Email { get; private set; }
+    public string Telefone { get; private set; }
     public DateTime DataCadastro { get; private set; }
-    
-    public Cliente()
+
+    public List<Endereco> Enderecos { get; private set; }
+    public List<Pedido> Pedidos { get; private set; }
+
+    public Cliente(string nome, string cpf, string email, string telefone)
     {
+        if (string.IsNullOrWhiteSpace(nome))
+            throw new Exception("Nome não pode ser vazio.");
+
+        if (string.IsNullOrWhiteSpace(cpf))
+            throw new Exception("Cpf não pode ser vazio.");
+
+        if (string.IsNullOrWhiteSpace(email) || !email.Contains("@"))
+            throw new Exception("E-mail inválido.");
+
+        if (string.IsNullOrWhiteSpace(telefone))
+            throw new Exception("Telefone não pode ser vazio.");
+
+        Nome = nome;
+        Cpf = cpf;
+        Email = email;
+        Telefone = telefone;
         DataCadastro = DateTime.UtcNow;
+        Enderecos = new List<Endereco>();
+        Pedidos = new List<Pedido>();
     }
 }
