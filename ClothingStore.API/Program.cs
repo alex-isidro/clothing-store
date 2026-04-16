337a92b151fd90f8e6ad60f6d3880c41ea91c537
alex-isidro <alexander.d.isidro@gmail.com>
2026-04-16 01:27:33 -0300

    feat: Adiciona configuração de conexão com o banco de dados Postgres

--- ClothingStore.API/Program.cs
+++ ClothingStore.API/Program.cs
@@ -1,3 +1,6 @@
+using ClothingStore.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 namespace ClothingStore.API;
 
 public class Program
@@ -6,7 +9,13 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
+        
+        builder.Services.AddDbContext<ClothingStoreContex>(options =>
+        {
+            options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"));
+        });
 
+        
         app.Run();
     }
 }
