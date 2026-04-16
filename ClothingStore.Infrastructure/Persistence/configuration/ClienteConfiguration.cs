012fe3f07165b57fb8cb45be0b73bd629c881c50
alex-isidro <alexander.d.isidro@gmail.com>
2026-04-16 01:28:46 -0300

    feat: Adiciona configurações de mapeamento para Cliente e Endereco

---
+++ ClothingStore.Infrastructure/Persistence/configuration/ClienteConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ClothingStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
+{
+    public void Configure(EntityTypeBuilder<Cliente> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Nome).IsRequired();
+        builder.Property(c => c.Cpf).IsRequired();
+        builder.Property(c => c.Email).IsRequired();
+        builder.Property(c => c.Telefone).IsRequired();
+        builder.Property(c => c.DataCadastro).IsRequired();
+
+        builder.HasIndex(c => c.Cpf).IsUnique();
+        builder.HasIndex(c => c.Email).IsUnique();
+    }
+}
---
+++ ClothingStore.Infrastructure/Persistence/configuration/EnderecoConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using ClothingStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class EnderecoConfiguration : IEntityTypeConfiguration<Endereco>
+{
+    public void Configure(EntityTypeBuilder<Endereco> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Logradouro).IsRequired();
+        builder.Property(e => e.Numero).IsRequired();
+        builder.Property(e => e.Bairro).IsRequired();
+        builder.Property(e => e.Cidade).IsRequired();
+        builder.Property(e => e.Estado).IsRequired();
+        builder.Property(e => e.Cep).IsRequired();
+
+        // N:1
+        builder.HasOne<Cliente>()
+            .WithMany()
+            .HasForeignKey(e => e.ClienteId)
+            .IsRequired();
+    }
+}
