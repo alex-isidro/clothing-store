8809ecb6036cc76a82442842e091589eb159e59c
alex-isidro <alexander.d.isidro@gmail.com>
2026-04-16 01:28:40 -0300

    feat: Adiciona configurações de mapeamento para Estoque e ItemPedido

---
+++ ClothingStore.Infrastructure/Persistence/configuration/EstoqueConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ClothingStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class EstoqueConfiguration : IEntityTypeConfiguration<Estoque>
+{
+    public void Configure(EntityTypeBuilder<Estoque> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.QuantidadeDisponivel).IsRequired();
+        builder.Property(e => e.QuantidadeMinima).IsRequired();
+
+        // 1:1
+        builder.HasOne<Produto>()
+            .WithOne()
+            .HasForeignKey<Estoque>(e => e.ProdutoId)
+            .IsRequired();
+    }
+}
---
+++ ClothingStore.Infrastructure/Persistence/configuration/ItemPedidoConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using ClothingStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ItemPedidoConfiguration : IEntityTypeConfiguration<ItemPedido>
+{
+    public void Configure(EntityTypeBuilder<ItemPedido> builder)
+    {
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.Quantidade).IsRequired();
+
+        builder.Property(i => i.PrecoUnitario)
+            .HasColumnType("decimal(10,2)");
+
+        builder.Property(i => i.Subtotal)
+            .HasColumnType("decimal(10,2)");
+
+        // N:1
+        builder.HasOne<Pedido>()
+            .WithMany()
+            .HasForeignKey(i => i.PedidoId)
+            .IsRequired();
+
+        // N:1
+        builder.HasOne<Produto>()
+            .WithMany()
+            .HasForeignKey(i => i.ProdutoId)
+            .IsRequired();
+    }
+}
