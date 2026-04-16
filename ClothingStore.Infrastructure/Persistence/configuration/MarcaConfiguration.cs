9ecdb92a43a0eb38cd6b361c519d831f09c97853
alex-isidro <alexander.d.isidro@gmail.com>
2026-04-16 01:28:16 -0300

    feat: Adiciona configurações de mapeamento para Marca e Pagamento

---
+++ ClothingStore.Infrastructure/Persistence/configuration/MarcaConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using ClothingStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class MarcaConfiguration : IEntityTypeConfiguration<Marca>
+{
+    public void Configure(EntityTypeBuilder<Marca> builder)
+    {
+        builder.HasKey(m => m.Id);
+
+        builder.Property(m => m.Nome).IsRequired();
+        builder.Property(m => m.Descricao).IsRequired();
+    }
+}
---
+++ ClothingStore.Infrastructure/Persistence/configuration/PagamentoConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using ClothingStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class PagamentoConfiguration : IEntityTypeConfiguration<Pagamento>
+{
+    public void Configure(EntityTypeBuilder<Pagamento> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.TipoPagamento).IsRequired();
+        builder.Property(p => p.StatusPagamento).IsRequired();
+
+        builder.Property(p => p.Valor)
+            .HasColumnType("decimal(10,2)");
+
+        builder.Property(p => p.DataPagamento).IsRequired();
+
+        // 1:1
+        builder.HasOne<Pedido>()
+            .WithOne()
+            .HasForeignKey<Pagamento>(p => p.PedidoId)
+            .IsRequired();
+    }
+}
