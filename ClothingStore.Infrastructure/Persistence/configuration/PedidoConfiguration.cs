afe5561a46f66d46319d810756979f1f2410bba4
alex-isidro <alexander.d.isidro@gmail.com>
2026-04-16 01:27:54 -0300

    feat: Adiciona configurações de mapeamento para Pedido e Produto

---
+++ ClothingStore.Infrastructure/Persistence/configuration/PedidoConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using ClothingStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class PedidoConfiguration : IEntityTypeConfiguration<Pedido>
+{
+    public void Configure(EntityTypeBuilder<Pedido> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.DataPedido).IsRequired();
+        builder.Property(p => p.Status).IsRequired();
+
+        builder.Property(p => p.ValorTotal)
+            .HasColumnType("decimal(10,2)");
+
+        // N:1
+        builder.HasOne<Cliente>()
+            .WithMany()
+            .HasForeignKey(p => p.ClienteId)
+            .IsRequired();
+
+        // N:1
+        builder.HasOne<Endereco>()
+            .WithMany()
+            .HasForeignKey(p => p.EnderecoEntregaId)
+            .IsRequired();
+    }
+}
---
+++ ClothingStore.Infrastructure/Persistence/configuration/ProdutoConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using ClothingStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
+{
+    public void Configure(EntityTypeBuilder<Produto> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Nome).IsRequired();
+        builder.Property(p => p.Descricao).IsRequired();
+
+        builder.Property(p => p.Preco)
+            .HasColumnType("decimal(10,2)");
+
+        builder.Property(p => p.Tamanho).IsRequired();
+        builder.Property(p => p.Cor).IsRequired();
+
+        // N:1
+        builder.HasOne<Categoria>()
+            .WithMany()
+            .HasForeignKey(p => p.CategoriaId)
+            .IsRequired();
+
+        // N:1
+        builder.HasOne<Marca>()
+            .WithMany()
+            .HasForeignKey(p => p.MarcaId)
+            .IsRequired();
+    }
+}
