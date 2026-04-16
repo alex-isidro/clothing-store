bdaf8a75aace4ca4529558b544c0a35293ae33c1
alex-isidro <alexander.d.isidro@gmail.com>
2026-04-16 01:28:55 -0300

    feat: Adiciona configurações de mapeamento para Categoria

---
+++ ClothingStore.Infrastructure/Persistence/configuration/CategoriaConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using ClothingStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class CategoriaConfiguration : IEntityTypeConfiguration<Categoria>
+{
+    public void Configure(EntityTypeBuilder<Categoria> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Nome).IsRequired();
+        builder.Property(c => c.Descricao).IsRequired();
+    }
+}
